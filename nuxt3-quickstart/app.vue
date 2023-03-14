259a1cf16d881d8f47a11a86f5731953aaaa14ad
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-03-14 10:39:50 +0300

    Nuxt3 quickstart added

---
+++ nuxt3-quickstart/app.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="container">
+		<div class="flex flex-column align-items-center mt-1">
+			<img alt="Vue logo" src="./assets/primevue-logo.png" />
+			<Toast />
+
+			<div class="mt-4">
+				<form @submit.prevent="greet">
+					<InputText type="text" v-model="text" />
+					<Button type="submit" label="Submit" />
+				</form>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useToast } from "primevue/usetoast";
+
+const text = ref();
+const toast = useToast();
+const greet = () => {
+	toast.add({ severity: "info", summary: "Hello " + text.value });
+};
+</script>
+
+<style>
+.container {
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
---
+++ nuxt3-quickstart/plugins/primevue.js
@@ -0,0 +1,14 @@
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ToastService)
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Toast', Toast)
+    //other components that you need
+})
