a8f951f38d15ee78f49a65862e2df940354a72db
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-04-09 19:30:08 +0300

    vue-i18n sample added

---
+++ vue-i18n/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="flex flex-column justify-content-center align-items-center h-screen"
+  >
+    <I18nSample />
+  </div>
+</template>
+
+<script setup>
+import I18nSample from "./components/I18nSample.vue";
+</script>
+
+<style></style>
