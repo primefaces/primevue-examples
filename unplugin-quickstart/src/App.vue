fc1a5d03e34be3abae7c1b0b70438e7be9fc8e02
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-09-27 12:46:35 +0300

    unplugin-vue-components quickstart added

---
+++ unplugin-quickstart/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <img alt="Vue logo" src="./assets/primevue-logo.png" height="90" />
+      <form @submit.prevent="greet">
+        <InputText type="text" v-model="text" placeholder="Name" />
+        <Button type="submit" label="Submit" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const text = ref();
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--surface-ground);
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+  border: 1px solid var(--surface-border);
+  background-color: var(--surface-card);
+  padding: 3rem;
+  border-radius: 1rem;
+}
+
+form {
+  display: flex;
+  gap: 1rem;
+}
+</style>
---
+++ unplugin-quickstart/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "primevue/resources/themes/lara-dark-teal/theme.css";
+
+const app = createApp(App);
+
+app.mount("#app");
