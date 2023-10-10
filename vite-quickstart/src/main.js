60a1d7c07c6bbbe9b4c84976709d4e5bd49bb441
Furkan Sezis <sezisfurkan24@gmail.com>
2023-10-10 09:33:15 +0300

    Vite quickstart update

--- vite-quickstart/src/main.js
+++ vite-quickstart/src/main.js
@@ -1,16 +1,18 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
 
-import '@/assets/main.css'
-import 'primevue/resources/themes/lara-light-teal/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';          
+import 'primevue/resources/themes/lara-dark-teal/theme.css'
 
-const app = createApp(App);
-app.use(PrimeVue);
+import PrimeVue from 'primevue/config'
 
-app.component('Button', Button);
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+const app = createApp(App)
 
-app.mount('#app');
+app.use(PrimeVue, { ripple: true })
+
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.mount('#app')
