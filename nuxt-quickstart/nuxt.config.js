ac7ad50b5a8a13847a56c99f11d62d525dc63c4a
Furkan Sezis <sezisfurkan24@gmail.com>
2023-10-13 17:28:26 +0300

    Nuxt quickstart updated

--- nuxt-quickstart/nuxt.config.js
+++ nuxt-quickstart/nuxt.config.js
@@ -1,9 +1,17 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Nuxt App",
+    },
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  devtools: { enabled: false },
   css: [
-    'primevue/resources/themes/lara-dark-teal/theme.css'
+    "primevue/resources/themes/lara-dark-teal/theme.css",
+    "@/assets/main.css",
+    "primeicons/primeicons.css",
   ],
   build: {
-    transpile: ['primevue']
-  }
-})
+    transpile: ["primevue"],
+  },
+});
--- nuxt-quickstart/plugins/primevue.js
+++ nuxt-quickstart/plugins/primevue.js
@@ -1,14 +1,11 @@
-import PrimeVue from 'primevue/config'
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import Toast from 'primevue/toast'
-import ToastService from 'primevue/toastservice'
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
 
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
-    nuxtApp.vueApp.use(ToastService)
-    nuxtApp.vueApp.component('Button', Button)
-    nuxtApp.vueApp.component('InputText', InputText)
-    nuxtApp.vueApp.component('Toast', Toast)
-    //other components that you need
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+  nuxtApp.vueApp.component("Button", Button);
+  nuxtApp.vueApp.component("InputText", InputText);
+
+  //other components that you need
+});
