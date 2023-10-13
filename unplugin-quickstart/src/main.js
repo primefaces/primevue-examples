234fbcfb88def945931b07dbad80a59e52fdcb2e
Furkan Sezis <sezisfurkan24@gmail.com>
2023-10-13 17:45:45 +0300

    unplagin-quickstart updated

--- unplugin-quickstart/src/main.js
+++ unplugin-quickstart/src/main.js
@@ -1,4 +1,8 @@
+import "./assets/main.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
+
+import "primevue/resources/themes/lara-dark-teal/theme.css";
 
 createApp(App).mount("#app");
