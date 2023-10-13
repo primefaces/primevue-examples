379b36c7823732c591b1d3ec661d4fb1221dda0f
Furkan Sezis <sezisfurkan24@gmail.com>
2023-10-13 18:05:10 +0300

    Path Fixes

--- vite-quickstart/src/App.vue
+++ vite-quickstart/src/App.vue
@@ -154,20 +154,25 @@ function startAnimation() {
 
     <div class="footer">
       <div class="footer-content">
-        <a href="" class="license">PrimeVue - MIT License</a>
+        <a
+          href="https://github.com/primefaces/primevue/blob/master/LICENSE.md"
+          target="_blank"
+          class="license"
+          >PrimeVue - MIT License</a
+        >
 
         <a class="footer-logo">
           <img src="./assets/PrimeVue.svg" alt="logo" />
         </a>
 
         <div class="socials">
-          <a href="">
+          <a href="https://github.com/primefaces/primevue" target="_blank">
             <i class="pi pi-github"></i>
           </a>
-          <a href="">
+          <a href="https://discord.gg/j2DQFtPD8z" target="_blank">
             <i class="pi pi-discord"></i>
           </a>
-          <a href="">
+          <a href="https://twitter.com/primevue" target="_blank">
             <i class="pi pi-twitter"></i>
           </a>
         </div>
