8eaef0626bbb58ecc16d5bcee57733ee3c34e093
Cagatay Civici <cagatay.civici@gmail.com>
2023-12-19 18:50:36 +0300

    Update Vite Unstyled TW sample

--- vite-unstyled-tailwind/tailwind.config.js
+++ vite-unstyled-tailwind/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-        "./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}",
-    ],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
