b54d8fbf3761e40b21f223b12daf4e0ef323080a
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-06-14 10:43:34 +0300

    Update for Nuxt styled example for v4

--- nuxt-styled-tailwind/nuxt.config.ts
+++ nuxt-styled-tailwind/nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
-  primevue: {
-    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
-    components: {
-      exclude: ["Editor", "Chart"]
-    }
-  },
-  css: ["primevue/resources/themes/lara-dark-indigo/theme.css"]
+    devtools: { enabled: true },
+    modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+    primevue: {
+        usePrimeVue: true,
+        importTheme: { from: "@/themes/my-theme.js" },
+        components: {
+            exclude: ["Editor", "Chart"],
+        },
+    },
 });
