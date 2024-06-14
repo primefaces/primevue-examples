bc257dd1c585d30e9e836c97e617377925e50de6
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-06-14 10:43:14 +0300

    Remove old examples

--- unplugin-quickstart/src/main.js
+++
@@ -1,8 +0,0 @@
-import "./assets/main.css";
-
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import "primevue/resources/themes/lara-dark-teal/theme.css";
-
-createApp(App).mount("#app");
