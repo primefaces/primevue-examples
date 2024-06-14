cda89d9cdea04c36ecb55c0e31dadcfc1c932591
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-06-14 18:52:13 +0300

    Use @primevue/themes

--- nuxt-styled-tailwind/nuxt.config.ts
+++ nuxt-styled-tailwind/nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
+    devtools: { enabled: false },
     modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
     primevue: {
-        usePrimeVue: true,
         importTheme: { from: "@/themes/my-theme.js" },
         components: {
             exclude: ["Editor", "Chart"],
