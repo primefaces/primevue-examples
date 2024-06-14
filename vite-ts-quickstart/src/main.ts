e0daa8cd19e22b481ee16f00efc0f7947603443b
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-06-14 10:42:37 +0300

    Update Vite TS example for v4

--- vite-ts-quickstart/src/main.ts
+++ vite-ts-quickstart/src/main.ts
@@ -1,20 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
-import Toast from 'primevue/toast';
-import ToastService from 'primevue/toastservice';
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import Button from "primevue/button";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+import App from "./App.vue";
 
-import './assets/main.css'
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';          
+import "./assets/main.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
-app.use(PrimeVue);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+});
 app.use(ToastService);
 
-app.component('Button', Button);
-app.component('Toast', Toast);
+app.component("Button", Button);
+app.component("Toast", Toast);
 
-app.mount('#app');
+app.mount("#app");
