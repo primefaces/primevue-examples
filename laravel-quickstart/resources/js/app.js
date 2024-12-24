b7434e0bebcbbb5bab7904a021f499488f1a781f
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-12-24 14:17:21 +0300

    Laravel quickstart initial commit

---
+++ laravel-quickstart/resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
---
+++ laravel-quickstart/resources/js/components/App.vue
@@ -0,0 +1,1 @@
+<template>Vue project</template>
