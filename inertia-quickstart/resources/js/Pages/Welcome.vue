97b2822a3094629e3c086c9a8f3bf06298c4f2ea
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-12-25 12:02:01 +0300

    Inertia quickstart added

---
+++ inertia-quickstart/resources/js/Pages/Welcome.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const text = ref(null);
+const msg = ref(null);
+
+function greet() {
+    msg.value = "Hello " + text.value;
+}
+</script>
+
+<template>
+    <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen p-10">
+        <div class="flex items-center justify-center min-h-screen p-10">
+            <section class="bg-white dark:bg-surface-900 p-10 rounded-xl flex flex-col gap-8 max-w-3xl">
+                <h1 class="text-4xl text-black dark:text-white font-bold text-center">Tailwind CSS + PrimeVue</h1>
+                <div class="flex gap-4">
+                    <InputText v-model="text" maxlength="20" fluid />
+                    <Button label="Submit" @click="greet" class="min-w-20" :disabled="!text" />
+                </div>
+                <div class="h-8 text-center text-primary text-xl">
+                    {{ msg }}
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
---
+++ inertia-quickstart/resources/js/app.js
@@ -0,0 +1,38 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
+            .component('Button', Button)
+            .component('InputText', InputText)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
