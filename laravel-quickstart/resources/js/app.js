12cfcbc7a1bc5028d6111ed8cdc3ba318715be32
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-02-26 20:07:27 +0300

    Update for laravel-quickstart

--- laravel-quickstart/resources/js/app.js
+++ laravel-quickstart/resources/js/app.js
@@ -2,19 +2,18 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './components/App.vue';
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App);
 
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+            darkModeSelector: '.p-dark'
+        }
     }
 });
 
-app.component('Button', Button);
-app.component('InputText', InputText);
 
 app.mount('#app');
--- laravel-quickstart/vite.config.js
+++ laravel-quickstart/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +11,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue()
+        vue(),
+        tailwindcss(),
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
+        })
     ],
 });
