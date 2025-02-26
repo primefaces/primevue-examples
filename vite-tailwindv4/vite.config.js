a32af98520641cb78d1b1c4bd2354553821f4211
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-02-26 16:26:09 +0300

    Update vite-tailwindv4 example

--- vite-tailwindv4/vite.config.js
+++ vite-tailwindv4/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
 import tailwindcss from "@tailwindcss/vite";
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
+  ]
 });
