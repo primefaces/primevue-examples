fe90520ec393e429b778634d2839f5d7633f5606
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-02-26 10:57:26 +0300

    Update nuxt-quickstart example

--- nuxt-quickstart/nuxt.config.js
+++ nuxt-quickstart/nuxt.config.js
@@ -1,25 +1,34 @@
-import Aura from '@primevue/themes/aura';
+import Aura from '@primeuix/themes/aura';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "Nuxt App",
-    },
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
-  devtools: { enabled: false },
-  css: [
-    "@/assets/main.css",
-    "primeicons/primeicons.css",
-  ],
-  modules: ["@primevue/nuxt-module"],
-  primevue: {
-    options: {
-      theme: {
-        preset: Aura
-      },
-      ripple: true
-    },
-    autoImport: true
-  }
+	app: {
+		head: {
+			title: "Nuxt App",
+		},
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
+	devtools: { enabled: false },
+	css: [
+		"@/assets/main.css"
+	],
+	modules: ["@primevue/nuxt-module"],
+	primevue: {
+		options: {
+			theme: {
+				preset: Aura,
+				options: {
+					darkModeSelector: ".p-dark",
+				}
+			},
+			ripple: true
+		},
+		autoImport: true
+	},
+	vite: {
+		plugins: [
+			tailwindcss(),
+		]
+	},
+	compatibilityDate: "2025-02-26"
 });
